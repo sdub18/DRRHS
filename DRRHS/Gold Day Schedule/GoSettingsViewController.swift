9ba8b099b3b52631710eb390eff1c93870b2d021
Sam Dubois <samdubois18@icloud.com>
2017-08-28 22:54:40 -0400

    minor touch ups

---
+++ DRRHS/Gold Day Schedule/GoSettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GoSettingsViewController.swift
+//  DRRHS
+//
+//  Created by Sam DuBois on 8/28/17.
+//  Copyright © 2017 Joshua Holme. All rights reserved.
+//
+
+import UIKit
+
+class GoSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
